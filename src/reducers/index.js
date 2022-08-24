243eb1b3c3d722a35fa402d5a63cc7a6277b4cbb
Alex Cattell <abcattell91@gmail.com>
2022-08-24 19:59:45 +0100

    blog names are appearing on blogs now

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import postsReducer from './postsReducer'
 import { combineReducers } from 'redux';
+import usersReducer from './usersReducer'
 
 export default combineReducers({
   posts: postsReducer,
+  users: usersReducer
 });
